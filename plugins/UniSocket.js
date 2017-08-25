9d34f096fd2fcdb863052cad8489185cf29720d2
FreedomKnight <fk@unisharp.com>
2017-08-25 12:44:36 +0800

    修了一些小東西 讓 let run 讓 console hide

--- plugins/UniSocket.js
+++ plugins/UniSocket.js
@@ -1,4 +1,6 @@
 import Timer from '~/plugins/Timer'
+import debugModule from 'debug'
+let debug = debugModule('UniSocket')
 
 class UniSocket {
   constructor (wsUri) {
@@ -10,33 +12,31 @@ class UniSocket {
   }
 
   connect () {
-    var events = this.events
+    let events = this.events
 
     this.socket = new WebSocket(this.wsUri)
     this.socket.onopen = function (ev) {
-      console.log('connected')
+      debug('connected')
       // getNote(note_id)
       events['open']()
     }
 
-    var that = this
-
     this.socket.onclose = function (ev) {
-      console.log('Connection lost.')
+      debug('Connection lost.')
 
       new Timer(5)
         .countDown((seconds) => {
           console.log('Retry connection in : ' + seconds + ' seconds')
         })
         .complete(() => {
-          that.connect()
+          this.connect()
           console.log('restarting')
         })
         .start()
     }
 
     this.socket.onerror = function (ev) {
-      console.log(ev)
+      debug(ev)
     }
   }
 
@@ -49,16 +49,16 @@ class UniSocket {
   on (eventName, callback) {
     this.events[eventName] = callback
 
-    var events = this.events
+    let events = this.events
 
     this.socket.onmessage = function (ev) {
-      var data = JSON.parse(ev.data)
+      let data = JSON.parse(ev.data)
       if (events[data.action] !== undefined) {
         events[data.action](data)
       }
-      console.log('message: ' + ev.data)
+      debug('message: ' + ev.data)
     }
-    console.log('Event received : ' + eventName)
+    debug('Event received : ' + eventName)
   }
 }
 
