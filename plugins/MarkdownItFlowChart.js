1a068e6b66dea651f24d40d8677547b26227137c
FreedomKnight <fk@unisharp.com>
2017-08-29 15:14:35 +0800

    fix workflow

--- plugins/MarkdownItFlowChart.js
+++ plugins/MarkdownItFlowChart.js
@@ -1,9 +1,10 @@
 const wrap = render =>
-  function (...args) {
+  function (tokens, index, options, env, self) {
     if (process.browser) {
       const flowchart = require('flowchart.js')
-      let token = args[0][0]
+      let token = tokens[index]
       let diagram = null
+      console.log(token)
       switch (token.info) {
         case 'flow':
         case 'flowchart':
@@ -11,19 +12,22 @@ const wrap = render =>
           break
       }
 
-      let id = `diagram-${Math.random() * 100}`
-      // 這裡是故意異步的唷
-      Promise.resolve().then(() => {
-        diagram.drawSVG(id)
-      })
-      return `<div id='${id}'></div>`
+      if (['flow', 'flowchart'].includes(token.info)) {
+        let id = `diagram-${Math.random() * 100}`
+        // 這裡是故意異步的唷
+        Promise.resolve().then(() => {
+          diagram.drawSVG(id)
+        })
+
+        token.content = ''
+        token.attrJoin('id', id)
+      }
     }
 
-    return render.apply(this, args)
+    return render.apply(this, arguments)
   }
 
 const flowchartjs = (md, opts) => {
-  // opts = Object.assign({}, highlightjs.defaults, opts)
   md.renderer.rules.fence = wrap(md.renderer.rules.fence)
 }
 
